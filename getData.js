84091cf73260654371471b9bfbea4fd8df2bdd9e
LinoyHorvat <linoy.horvat@gmail.com>
2022-08-02 16:59:23 +0300

    with trie-search, nocheat del function

--- getData.js
+++ getData.js
@@ -1,7 +1,7 @@
 const fs = require("fs-extra");
 const async = require("async");
 const csv = require("csv-parser");
-const TrieSearch = require('trie-search');
+const TrieSearch = require("trie-search");
 
 // TODO: understand what csv-parser is & createReadStream & pipe
 
@@ -15,13 +15,14 @@ constants
 const userHashByID = new Map();
 const userHashByCountry = new Map();
 const userHashByYear = new Map();
+const userHashByFullName = new Map();
 // const userTrie = new TrieSearch("name",{min: 3});
 const userTrie = new TrieSearch();
 
 /*----------------------------------------------------------------
 initiates 
 /*----------------------------------------------------------------*/
-// TODO: description of the function
+// TODO: go over all the description of the functions
 /**
  * @function readDataFromCsvFileAndInitiate
  * reads the data from the CSV file and initiates.
@@ -37,7 +38,8 @@ function readDataFromCsvFileAndInitiate() {
       addUserToId(user);
       addUserToCountry(user);
       addUserToYear(user);
-      addUserToTrie(user)
+      addUserToTrie(user);
+      addUserToFullName(user);
     })
     .on("end", function () {
       console.log("Data loaded");
@@ -95,9 +97,20 @@ const addUserToYear = (user) => {
  * add user to addUserToTrie trie
  */
 const addUserToTrie = (user) => {
-  const name = user.name.replace(' ','%20')
-  console.log(user.id,name);
-  userTrie.map(name,user.id)
+  userTrie.map(user.name, user.id);
+};
+/**
+ * @function addUserToFullName
+ * add user to addUserToFullName hash
+ */
+const addUserToFullName = (user) => {
+  const name = user.name.replace(" ", "").toLowerCase();
+  // check if fullName already exists and add user to userHashByFullName
+  if (userHashByFullName.get(name)) {
+    const arrOfId = userHashByFullName.get(name);
+    arrOfId.push(user.id);
+    userHashByFullName.set(name, arrOfId);
+  } else userHashByFullName.set(name, [user.id]);
 };
 /*----------------------------------------------------------------
 get 
@@ -117,7 +130,7 @@ const getUserById = (id) => {
 const getUserByCountry = (country) => {
   country = country.toLowerCase();
   return userHashByCountry.get(country)
-    ? userHashByCountry.get(country)
+    ? returnUserFunction("country",country)
     : "Country dosen't exist";
 };
 /**
@@ -128,17 +141,55 @@ const getUserByAge = (age) => {
   const year = new Date().getFullYear() - age;
   console.log(year);
   return userHashByYear.get(year)
-    ? userHashByYear.get(year)
+    ? returnUserFunction("year",year)
     : "Age dosen't exist";
 };
 /**
  * @function getUserByName
- * get user by Age
+ * get user by name
  */
-// TODO: fix first and last name issue 
 const getUserByName = (name) => {
-  name = name.replace(' ','%20')
-  return userTrie.search(name);
+  // check if it is a full name => call Hash of full name
+  if (name.indexOf(" ") >= 0) {
+    fullName = name.replace(" ", "").toLowerCase();
+    return userHashByFullName.get(fullName)
+      ? returnUserFunction("fullName", fullName)
+      : "user dosen't exist";
+  }
+  // not a full name => call trie
+  return returnUserFunction("name", name);
+};
+/**
+ * @function returnUserFunction
+ * get id array and return user obj
+ */
+const returnUserFunction = (op, val) => {
+  const returnArr = [];
+  let arrOfId;
+  switch (op) {
+    case "name": {
+      arrOfId = userTrie.search(val).splice(0);
+      break;
+    }
+    case "fullName": {
+      const fullName = val.replace(" ", "").toLowerCase();
+      arrOfId = userHashByFullName.get(fullName);
+      break;
+    }
+    case "year": {
+      arrOfId = userHashByYear.get(val);
+      break;
+    }
+    case "country": {
+      arrOfId = userHashByCountry.get(val);
+      break;
+    }
+  }
+  return arrOfId
+    ? arrOfId.map((id) => {
+        if (userHashByID.get(id)) return userHashByID.get(id);
+      })
+    : "no user found";
 };
 
 /*----------------------------------------------------------------
@@ -158,6 +209,7 @@ const deleteUser = (id) => {
   if (user) {
     deleteUserFromHashCountry(user.country, id);
     deleteUserFromHashAge(user.dob, id);
+    deleteUserFromHashFullName(user.name, id);
     userHashByID.delete(id);
   } else return "User id dosen't exist";
 };
@@ -168,9 +220,11 @@ const deleteUser = (id) => {
 const deleteUserFromHashCountry = (country, id) => {
   country = country.toLowerCase();
   const arrOfId = userHashByCountry.get(country);
-  const index = arrOfId.indexOf(id)
+  const index = arrOfId.indexOf(id);
   if (index > -1) arrOfId.splice(index, 1);
-  userHashByCountry.set(country, arrOfId);
+  arrOfId.length === 0
+    ? userHashByCountry.delete(country)
+    : userHashByCountry.set(country, arrOfId);
 };
 /**
  * @function deleteUserFromHashAge
@@ -179,9 +233,25 @@ const deleteUserFromHashCountry = (country, id) => {
 const deleteUserFromHashAge = (dob, id) => {
   const userYear = getUserYear(dob);
   const arrOfId = userHashByYear.get(userYear);
-  const index = arrOfId.indexOf(id)
+  const index = arrOfId.indexOf(id);
   if (index > -1) arrOfId.splice(index, 1);
-  userHashByYear.set(userYear, arrOfId);
+  arrOfId.length === 0
+    ? userHashByYear.delete(userYear)
+    : userHashByYear.set(userYear, arrOfId);
+};
+/**
+ * @function deleteUserFromHashFullName
+ * delete from userHashByFullName
+ */
+const deleteUserFromHashFullName = (name, id) => {
+  fullName = name.replace(" ", "").toLowerCase();
+  const arrOfId = userHashByFullName.get(fullName);
+  console.log(arrOfId);
+  const index = arrOfId.indexOf(id);
+  if (index > -1) arrOfId.splice(index, 1);
+  arrOfId.length === 0
+    ? userHashByFullName.delete(fullName)
+    : userHashByFullName.set(fullName, arrOfId);
 };
 
 /*----------------------------------------------------------------
@@ -194,5 +264,5 @@ module.exports = {
   getUserByCountry,
   getUserByAge,
   deleteUser,
-  getUserByName
+  getUserByName,
 };
